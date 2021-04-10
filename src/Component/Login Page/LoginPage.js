c9e0e4f11b412322223a64816d7463d895c53c97
Samaresh Samanta <ss9261@srmist.edu.in>
2021-04-10 21:38:17 +0530

    done one side of the login page

--- src/Component/Login Page/LoginPage.js
+++ src/Component/Login Page/LoginPage.js
@@ -1,9 +1,30 @@
 import react from "react";
 import classes from "../Login Page/LoginPage.module.css";
+import Logo from "../../Assets/Logo1.svg";
+import {Link} from "react-router-dom";
 
 const LoginPage = ()=>{
     return(
         <div className={classes.majorContainer}>
+
+            <div className={classes.main}>
+                <div className={classes.formContainer}>
+
+                </div>
+                <div className={classes.logoContainer}>
+                    <div className={classes.logoQuote}>
+                        <img src={Logo} />
+                        <p>your wallpaper, your attitude</p>
+                    </div>
+                    <div className={classes.miniFooter}>
+                        <ul>
+                            <li style={{borderRight:"1px solid rgba(255, 255, 255, 0.445)"}}><Link to="/ContactUs" className={classes.options}>Contact Us</Link></li>
+                            <li><Link to="/AboutUs" className={classes.options}>About Us</Link></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     )
