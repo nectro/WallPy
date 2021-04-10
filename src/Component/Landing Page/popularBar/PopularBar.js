55e42a998c6d01363fb70557b0ae0b8f49957bfb
Samaresh Samanta <ss9261@srmist.edu.in>
2021-04-10 14:14:25 +0530

    adjusted the popular Bar

--- src/Component/Landing Page/popularBar/PopularBar.js
+++ src/Component/Landing Page/popularBar/PopularBar.js
@@ -5,13 +5,13 @@ const PopularBar = ()=> {
           <div className={classes.majorContainer}>
                
                <ul className={classes.optionslist}>
-                         <li className={classes.options} style={{borderRight: "1px solid #ffff"}}>wallpaper</li>
-                         <li className={classes.options}>trending</li>
+                         <li className={classes.options} style={{borderRight: "1px solid #ffff", paddingLeft: "0px",}}>trending</li>
+                         <li className={classes.options}>tech</li>
                          <li className={classes.options} >dark</li>
                          <li className={classes.options}>game</li>
                          <li className={classes.options}>car</li>
                          <li className={classes.options}>architecture</li>
-                         <li className={classes.options}>light painting</li>
+                         <li className={classes.options}>light</li>
                          <li className={classes.options} >religion</li>
                          <li className={classes.options}>stairs</li>
                          <li className={classes.options}>animal</li>
