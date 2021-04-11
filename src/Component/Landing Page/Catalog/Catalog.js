168dc16df852b7300cefd13e836bf007e2309dce
Shubham-Samanta <77207634+Shubham-Samanta@users.noreply.github.com>
2021-04-11 10:15:19 +0530

    added grid system

--- src/Component/Landing Page/Catalog/Catalog.js
+++ src/Component/Landing Page/Catalog/Catalog.js
@@ -1,1 +1,12 @@
 import React from "react"
+import classes from "../Catalog/Catalog.module.css"
+const catalog = () => {
+     return (
+          <div className={classes.majorContainer}>
+               <div className={classes.Column1}>picture1</div>
+               <div className={classes.Column2}>picture 2</div>
+               <div className={classes.Column3}>picture3</div>
+          </div>
+     )
+}
+export default catalog
--- src/Component/Landing Page/LandingPage.js
+++ src/Component/Landing Page/LandingPage.js
@@ -3,12 +3,14 @@ import classes from "../Landing Page/LandingPage.module.css";
 import Header from "../Landing Page/Header/Header";
 import PopularBar from "./popularBar/PopularBar"
 import BackgroundImage from "./BackgroundImage/BackgroundImage"
+import Catalog from "./Catalog/Catalog"
 const LandingPage = ()=>{
     return(
         <div className={classes.majorContainer}>
             <Header />
             <PopularBar />
             <BackgroundImage />
+            <Catalog />
         </div>
     )
 }
