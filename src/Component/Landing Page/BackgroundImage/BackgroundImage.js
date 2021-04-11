e19e2aa85f466df3f98a75230828b3bc244e43a7
Samaresh Samanta <ss9261@srmist.edu.in>
2021-04-11 21:12:32 +0530

    improvised the imgctn

--- src/Component/Landing Page/BackgroundImage/BackgroundImage.js
+++ src/Component/Landing Page/BackgroundImage/BackgroundImage.js
@@ -1,20 +1,20 @@
 import React from "react"
-import city from "../../../Assets/city.png"
 import Search from "../../../Assets/search.svg";
 import Logo from "../../../Assets/Logo1.svg";
 import classes from "../BackgroundImage/BackgroundImage.module.css"
 const BackgroundImage = () => {
   return (
-       <div className={classes.majorContainer} style={{ backgroundImage: `url(${city})`}}>
-            <div className={classes.searchBar}>
-                    
+       <div className={classes.majorContainer}>
+          <div className={classes.imgContainer}>
+               <div className={classes.searchBar}>          
                     <input type="text" className={classes.inputText}/>
                     <button className={classes.button}>
-                        <img src={Search} />
+                         <img src={Search} />
                     </button>
+               </div>
           </div>
           <div className={classes.bottomBar}></div>
-     </div>
+       </div>
      )
 }
 export default BackgroundImage
