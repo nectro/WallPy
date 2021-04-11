4835f0a19c2885bef6ee6c46e90053dd0459d78a
Shubham-Samanta <77207634+Shubham-Samanta@users.noreply.github.com>
2021-04-12 00:39:36 +0530

    upload component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import LandingPage from "../src/Component/Landing Page/LandingPage";
 import LoginPage from "../src/Component/Login Page/LoginPage";
+import Upload from './Component/Upload/Upload';
 //https://colorhunt.co/palette/253094 ------>the color theme used
 
 
@@ -15,6 +16,9 @@ function App() {
           </Route>
           <Route path="/Login" >
             <LoginPage />
+          </Route>
+          <Route path="/upload">
+            <Upload/>
           </Route>
         </Switch>
       </Router>
--- src/Component/Landing Page/LandingPage.js
+++ src/Component/Landing Page/LandingPage.js
@@ -4,6 +4,7 @@ import Header from "../Landing Page/Header/Header";
 import PopularBar from "./popularBar/PopularBar"
 import BackgroundImage from "./BackgroundImage/BackgroundImage"
 import Catalog from "./Catalog/Catalog"
+import Upload from "../Upload/Upload";
 const LandingPage = ()=>{
     return(
         <div className={classes.majorContainer}>
@@ -11,6 +12,7 @@ const LandingPage = ()=>{
             <PopularBar />
             <BackgroundImage />
             <Catalog />
+            <Upload/>
         </div>
     )
 }
