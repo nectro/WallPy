624b3acf8bda1ed8ae1b6575fc0c96df8f287fbf
Shubham-Samanta <77207634+Shubham-Samanta@users.noreply.github.com>
2021-04-15 15:56:08 +0530

    completed the about page

--- src/Component/AboutUs/AboutUs.js
+++ src/Component/AboutUs/AboutUs.js
@@ -1,6 +1,10 @@
 import React from "react"
 import classes from '../AboutUs/AboutUs.module.css'
 import Logo from '../../Assets/Logo.svg'
+import shubham from '../../Assets/shubham.svg'
+import shounak from '../../Assets/shounak 1.svg'
+import samy from '../../Assets/SAM 1.svg'
+import Footer from "../Landing Page/Footer/Footer"
 
 const AboutUs = () => {
      return (
@@ -17,15 +21,46 @@ const AboutUs = () => {
                          <div className={classes.content} >
                               Lorem ipsum demo quetes Lorem ipsum
                               demo quetes Lorem ipsum demo asd 
-                         quetes Lorem ipsum demo  quetes a </div>
+                         quetes Lorem ipsum demo  quetes a Lorem ipsum demo quetes Lorem ipsum
+                              demo quetes Lorem ipsum demo asd 
+                         quetes Lorem ipsum demo  quetes a Lorem ipsum demo quetes Lorem ipsum
+                              demo quetes Lorem ipsum demo asd 
+                         quetes Lorem ipsum demo  quetes a Lorem ipsum demo quetes Lorem ipsum
+                              demo quetes Lorem ipsum demo asd 
+                         quetes Lorem ipsum demo  quetes a Lorem ipsum demo quetes Lorem ipsum
+                              demo quetes Lorem ipsum demo asd 
+                         quetes Lorem ipsum demo  quetes a  </div>
                     </div>
                </div>
 
 
                <div className={classes.ourteam}>
-                    <div className={classes.title}>Our Team</div>
-                    <div ></div>
+                    <div className={classes.ourteamcontent}>
+                         <div className={classes.title}>Our Team</div>
+                         <div className={classes.content}>
+                              <div className={classes.memberinfo}>
+                                   <img className={classes.memberpic} src={samy} />
+                                   <p>Lorem ipsum demo quetes Lorem ipsum
+                              demo quetes Lorem ipsum demo asd 
+                         quetes Lorem ipsum demo  quetes a Lorem ipsum demo quetes</p>
+                              </div>
+                              <div className={classes.memberinfo} >
+                                   <p>Lorem ipsum demo quetes Lorem ipsum
+                                   demo quetes Lorem ipsum demo asd 
+                         quetes Lorem ipsum demo  quetes a Lorem ipsum demo quetes</p>
+                         <img className={classes.memberpic} src={shubham} />
+                              </div>
+                              <div className={classes.memberinfo}> 
+                                   <img className={classes.memberpic} src={shounak} />
+                                   <p>Lorem ipsum demo quetes Lorem ipsum
+                              demo quetes Lorem ipsum demo asd 
+                         quetes Lorem ipsum demo  quetes a Lorem ipsum demo quetes</p>
+                              </div>
+                         </div>
+                    </div>
+                    
                </div>
+               <Footer />
           </div>
      )
 }
