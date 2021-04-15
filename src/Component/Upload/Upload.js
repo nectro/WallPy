26dbabbd96302c86f17595b29a6edd2cf5ad6e7c
Samaresh Samanta <ss9261@srmist.edu.in>
2021-04-15 19:29:35 +0530

    setup firebase storage

--- src/Component/Upload/Upload.js
+++ src/Component/Upload/Upload.js
@@ -1,5 +1,6 @@
 import react, { useState } from "react"
 import ProgressBar from '../Upload/ProgressBar'
+import { projectStorage, firebaseApp } from "../firebase/Config"
 const Upload = () => {
      const [file, setFile] = useState(null);
      const [error, setError] = useState(null);
@@ -19,6 +20,21 @@ const Upload = () => {
                setError("please enter a valid file(png,jpeg");
                }
      }
+     const upload = (e)=>{
+          e.preventDefault();
+          console.log("yep!");
+          var storageRef = firebaseApp.storage().ref();
+
+          // Create a reference to 'mountains.jpg'
+          var mountainsRef = storageRef.child(file.name);
+
+          // Create a reference to 'images/mountains.jpg'
+          var mountainImagesRef = storageRef.child("images/+`${file}`");
+
+          mountainsRef.put(file).then((snapshot) => {
+               console.log('Uploaded a blob or file!');
+             });
+     }
      return (
           <div>
                <form>
@@ -28,6 +44,7 @@ const Upload = () => {
                          {file && <div className="file">{file.name}</div>}
                          {file && <ProgressBar file={file} setFile={setFile}/>}
                     </div>
+                    <button onClick={upload}>upload</button>
                </form>
                
           </div>
