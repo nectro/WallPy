5b0846e0dc07b73fd6947951c5787875f83cbd67
Samaresh Samanta <ss9261@srmist.edu.in>
2021-04-18 11:54:40 +0530

    done with the footer

--- src/Component/Landing Page/Footer/Footer.js
+++ src/Component/Landing Page/Footer/Footer.js
@@ -1,10 +1,24 @@
 import React from "react"
 import classes from "../Footer/Footer.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faCopyright } from '@fortawesome/free-regular-svg-icons'
+import {faFacebook, faInstagram, faMedium, faTwitter} from '@fortawesome/free-brands-svg-icons'
+
+
 const Footer = () => {
      return (
           <div className={classes.majorContainer}>
-               <div className={classes.copyright}> 2021 copyright </div>
-               <div className={classes.social}> Follow us on : socail media </div>
+               <div className={classes.copyright}>
+                    <FontAwesomeIcon icon={faCopyright} className={classes.icon}/>
+                     2021 copyright 
+               </div>
+               <div className={classes.social}> 
+                    Follow us on :
+                    <FontAwesomeIcon icon={faFacebook} className={classes.smicon1} />
+                    <FontAwesomeIcon icon={faInstagram} className={classes.smicon2} />
+                    <FontAwesomeIcon icon={faMedium} className={classes.smicon3} />
+                    <FontAwesomeIcon icon={faTwitter} className={classes.smicon4} />
+               </div>
           </div>
      )
 }
