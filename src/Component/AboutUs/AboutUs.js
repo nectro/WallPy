6892c38f2d60c019baade3f8625401c9606c075b
Samaresh Samanta <ss9261@srmist.edu.in>
2021-04-20 02:39:45 +0530

    about us animations

--- src/Component/AboutUs/AboutUs.js
+++ src/Component/AboutUs/AboutUs.js
@@ -9,6 +9,7 @@ import aboutUsVector from "../../Assets/abtVec.png"
 import Footer from "../Landing Page/Footer/Footer"
 import {faAngleDoubleDown} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Fade } from "react-awesome-reveal";
 
 const AboutUs = () => {
 
@@ -32,18 +33,19 @@ const AboutUs = () => {
                <div className={classes.aboutus} id="abt">
                     {/*<a href='https://www.freepik.com/vectors/business'>Business vector created by freepik - www.freepik.com</a> */}
                     <div className={classes.aboutcontent}>
-                         <div className={classes.image}>
-                              <a href='https://www.freepik.com/vectors/business'>
-                                   <img src={aboutUsVector} />
-                              </a>
-
-                         </div>
+                              <div className={classes.image}>
+                                   <a href='https://www.freepik.com/vectors/business'>
+                                        <img src={aboutUsVector} />
+                                   </a>
+                              </div>
                          <div className={classes.texts}>
                               <div className={classes.textContent}>
+                                   <Fade triggerOnce="true">
                                    <div className={classes.title} >About Us</div>
-                                   <div className={classes.abtContent} >
-                                   Welcome to Wallpy, your number one source for  of wallpapers. We're dedicated to providing you the best wallpapers for your device, with a focus on your likings, dependability and compatibility.
-                                   </div>
+                                        <div className={classes.abtContent} >
+                                        Welcome to Wallpy, your number one source for  of wallpapers. We're dedicated to providing you the best wallpapers for your device, with a focus on your likings, dependability and compatibility.
+                                        </div>
+                                   </Fade>
                               </div>
                          </div>
                     </div>
@@ -57,25 +59,37 @@ const AboutUs = () => {
                          </div>
                          <div className={classes.memberContent}>
                               <div className={classes.memberinfo}>
-                                   <img className={classes.memberpic} src={samy} />
-                                   <p>
-                                        <b>Samaresh Samanta</b><br/>
-                                        A student of SRM University, has a thirst of digging out new idea and implement them in real.
-                                   </p>
+                                   <Fade direction="left" triggerOnce="true">
+                                        <img className={classes.memberpic} src={samy} />
+                                   </Fade>
+                                   <Fade direction="right" triggerOnce="true">
+                                        <p>
+                                             <b>Samaresh Samanta</b><br/>
+                                             A student of SRM University, has a thirst of digging out new idea and implement them in real.
+                                        </p>
+                                   </Fade>
                               </div>
                               <div className={classes.memberinfo1} >
-                                   <img className={classes.memberpic} src={shubham} />
-                                   <p>
-                                        <b>Shubham Samanta</b><br/>
-                                        A student of NIT Durgapur, has a thirst of digging out new idea and implement them in real.
-                                   </p>
+                                   <Fade direction="right" triggerOnce="true">
+                                        <img className={classes.memberpic} src={shubham} />
+                                   </Fade>
+                                   <Fade direction="left" triggerOnce="true">
+                                        <p>
+                                             <b>Shubham Samanta</b><br/>
+                                             A student of NIT Durgapur, has a thirst of digging out new idea and implement them in real.
+                                        </p>
+                                   </Fade>
                               </div>
                               <div className={classes.memberinfo}> 
-                                   <img className={classes.memberpic} src={shounak} />
-                                   <p>
-                                        <b>Shounak Chatterjee</b><br/>
-                                        A student of SRM University, has a thirst of digging out new idea and implement them in real.
-                                   </p>
+                                   <Fade direction="left" triggerOnce="true">
+                                        <img className={classes.memberpic} src={shounak} />
+                                   </Fade>          
+                                   <Fade direction="right" triggerOnce="true"> 
+                                        <p>
+                                             <b>Shounak Chatterjee</b><br/>
+                                             A student of SRM University, has a thirst of digging out new idea and implement them in real.
+                                        </p>
+                                   </Fade>
                               </div>
                          </div>
                     </div>
