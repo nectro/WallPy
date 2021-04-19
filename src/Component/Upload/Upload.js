7380a26758cfcbf2ffdf340a10217ed48aaeb4f9
Shubham-Samanta <77207634+Shubham-Samanta@users.noreply.github.com>
2021-04-19 21:30:04 +0530

    setting iup upload url

--- src/Component/Upload/Upload.js
+++ src/Component/Upload/Upload.js
@@ -1,6 +1,6 @@
 import react, { useState, useEffect } from "react"
 import ProgressBar from '../Upload/ProgressBar'
-import { projectStorage, firebaseApp } from "../firebase/Config"
+import { projectStorage, firebaseApp ,projectfirestore} from "../firebase/Config"
 import classes from "../Upload/upload.module.css";
 import { useDropzone } from 'react-dropzone'
 
@@ -12,7 +12,7 @@ const Upload = () => {
      const [file, setFile] = useState(null);
      const [error, setError] = useState(null);
      const [url, setUrl] = useState(null);
-
+     const [firebaseurl, setfirebaseUrl] = useState(null);
      const [upstyle,setUpStyle] = useState({display:"block"});
      const [progstyle,setProgStyle] = useState({display:"none"});
 
@@ -25,17 +25,26 @@ const Upload = () => {
           setProgStyle({display:"block"});
 
           var storageRef = firebaseApp.storage().ref();
-
-          // Create a reference to 'images/mountains.jpg'
           var mountainImagesRef = storageRef.child(`images/+${file.name}`);
           
-
           mountainImagesRef .put(file).on('state_changed', (snap) => {
                let percentage = (snap.bytesTransferred / snap.totalBytes) * 100
                setProgress(percentage)
-             });
-     }
 
+          },
+               (err) => {
+                    setError(err);
+               }, async () => {
+                    const Url = await mountainImagesRef.getDownloadURL();
+                    console.log(Url)
+                    setfirebaseUrl(Url)
+                    
+               });
+     }
+     useEffect(()=> {
+          console.log(firebaseurl)
+     },[firebaseurl])
+     
      const onDrop = acceptedFiles => {
           var selected = acceptedFiles[0];
          
